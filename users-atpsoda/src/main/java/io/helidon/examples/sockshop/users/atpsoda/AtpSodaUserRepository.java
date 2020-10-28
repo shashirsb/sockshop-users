ae34cbd66f8303f5d09c32b67b06ae99be585863
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-28 15:32:57 +0530

    adding

--- users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
+++ users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
@@ -34,6 +34,65 @@ import org.eclipse.microprofile.opentracing.Traced;
 import static com.mongodb.client.model.Filters.eq;
 import static javax.interceptor.Interceptor.Priority.APPLICATION;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+
+import io.helidon.examples.sockshop.catalog.CatalogRepository;
+import io.helidon.examples.sockshop.catalog.DefaultCatalogRepository;
+
+
+import org.bson.BsonDocument;
+import org.bson.conversions.Bson;
+import org.eclipse.microprofile.opentracing.Traced;
+
+import io.helidon.examples.sockshop.catalog.atpsoda.AtpSodaProducers;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonArray;
+
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+import io.helidon.config.Config;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
+import io.helidon.webserver.Service;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import oracle.soda.rdbms.OracleRDBMSClient;
+import oracle.soda.OracleDatabase;
+import oracle.soda.OracleCursor;
+import oracle.soda.OracleCollection;
+import oracle.soda.OracleDocument;
+import oracle.soda.OracleException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * An implementation of {@link io.helidon.examples.sockshop.users.UserRepository}
  * that that uses MongoDB as a backend data store.
@@ -115,7 +174,7 @@ public class AtpSodaUserRepository extends DefaultUserRepository {
     @Override
     public Collection < ? extends User > getAllUsers() {
         List < User > results = new ArrayList < > ();
-        users.find().forEach((Consumer << ? super User > ) results::add);
+        // users.find().forEach((Consumer << ? super User > ) results::add);
         return results;
     }
 
