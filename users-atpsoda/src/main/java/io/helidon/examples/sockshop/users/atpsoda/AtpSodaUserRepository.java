82734182ff56358fc457d1f9ea2ab1edbe118375
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-28 21:53:34 +0530

    adding

--- users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
+++ users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
@@ -381,10 +381,20 @@ public class AtpSodaUserRepository extends DefaultUserRepository {
             System.out.println ("\n");
           
 
-            List < Address > addressesList = user.addresses;
-            List < Card > cardsList = user.cards;
+            // System.out.println()
 
-            String document = "{\"addresses\":" + addressesList.toString() + ",\"cards\":" + cardsList.toString() + ",\"email\":\"" + user.email + "\",\"firstName\":\"" + user.firstName + "\",\"lastName\":\"" + user.lastName + "\",\"links\":{\"customer\":{\"href\":\"http://user/customers/" + user.username + "\"},\"self\":{\"href\":\"http://user/customers/" + user.username + "\"},\"addresses\":{\"href\":\"http://user/customers/" + user.username + "/addresses\"},\"cards\":{\"href\":\"http://user/customers/" + user.username + "/cards\"}},\"password\":\"" + user.password + "\",\"username\":\"" + user.username + "\"}";
+            // JSONArray addressesList = user.addresses;
+
+            // for (int i = 0; i < addressesList.size(); i++) {
+            //                 addressesList.add(_jsonArrayaddresses.get(i).toString());
+            // }        
+            
+            // (JSONArray) jsonObject.get("addresses");
+
+            // List < Address > addressesList = user.addresses;
+            // List < Card > cardsList = user.cards;
+
+            String document = "{\"addresses\":" + user.addresses + ",\"cards\":" + user.cards + ",\"email\":\"" + user.email + "\",\"firstName\":\"" + user.firstName + "\",\"lastName\":\"" + user.lastName + "\",\"links\":{\"customer\":{\"href\":\"http://user/customers/" + user.username + "\"},\"self\":{\"href\":\"http://user/customers/" + user.username + "\"},\"addresses\":{\"href\":\"http://user/customers/" + user.username + "/addresses\"},\"cards\":{\"href\":\"http://user/customers/" + user.username + "/cards\"}},\"password\":\"" + user.password + "\",\"username\":\"" + user.username + "\"}";
             OracleDocument newDoc = this.db.createDocumentFromString(document);
 
             
