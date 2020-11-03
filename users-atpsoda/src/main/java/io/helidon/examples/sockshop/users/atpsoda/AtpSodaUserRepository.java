0d85ca30d511bb5203d153d998f153ff84e51420
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-11-03 10:38:08 +0530

    adding

--- users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
+++ users-atpsoda/src/main/java/io/helidon/examples/sockshop/users/atpsoda/AtpSodaUserRepository.java
@@ -320,7 +320,7 @@ public class AtpSodaUserRepository extends DefaultUserRepository {
                     JSONObject jsonObject = (JSONObject) obj;
                     
                     Gson gson = new Gson(); // Or use new GsonBuilder().create();
-                    User targetUser = gson.fromJson(resultDoc.getContentAsString(), User.class); // deserializes json into target2
+                    User targetUser = gson.fromJson(jsonObject, User.class); // deserializes json into target2
                     
                  
 
@@ -376,7 +376,7 @@ public class AtpSodaUserRepository extends DefaultUserRepository {
         }
         System.out.println("User findUser:" + user);
         System.out.println("User findUser. GET Request 200OK");
-        return user;
+        return targetUser;
     }
 
     private void updateUser(String userID, User user) {
